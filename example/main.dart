2f00de6e6440b0302985b22780f87f8cec7e3229
胡文涛 <35894003+Hu-Wentao@users.noreply.github.com>
2025-07-20 02:58:21 +0800

    add example

---
+++ example/main.dart
@@ -0,0 +1,15 @@
+import 'package:slowly/slowly.dart';
+
+main() {
+  final sly = Slowly<String>();
+
+  /// debounce
+  bool access1 =
+      sly.duration('foo', SlowlyTp.debounce, const Duration(milliseconds: 100));
+  bool access2 = sly.ms('foo', ms: 100, tp: SlowlyTp.debounce);
+  bool access3 = sly.seconds('foo', sec: 1, tp: SlowlyTp.debounce);
+
+  /// throttle
+  bool access4 =
+      sly.duration('foo', SlowlyTp.throttle, const Duration(milliseconds: 100));
+}
